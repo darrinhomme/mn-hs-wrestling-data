13010e02311f51b3f88b39e1de2bab3ff0739fe8
Darrin Homme <dhomme@lwcm.com>
2020-04-08 08:42:41 -0500

    added data

--- R/Weight-Distribution.R
+++ R/Weight-Distribution.R
@@ -1,27 +1,30 @@
-read.csv("Sandbox/Data/members.csv") %>% 
-  dplyr::filter(Grade %in% c("7th", "8th", "Fr.", "So.", "Jr.", "Sr.")) %>%
-  dplyr::filter(Sex == "M") %>%
-  dplyr::group_by(Weight) %>%
-  dplyr::summarise(Count = n()) %>%
+library(ggplot2)
+library(dplyr)
+
+read.csv("Data/members.csv") %>% 
+  filter(Grade %in% c("7th", "8th", "Fr.", "So.", "Jr.", "Sr.")) %>%
+  filter(Sex == "M") %>%
+  group_by(Weight) %>%
+  summarise(Count = n()) %>%
   ggplot(aes(x = as.character(Weight), y = Count)) +
     geom_bar(stat = "identity") +
     labs(title = "All Grades Distribution by Weight Class")
 
-read.csv("Sandbox/Data/members.csv") %>% 
-  dplyr::filter(Grade %in% c("Fr.", "So.", "Jr.", "Sr.")) %>%
-  dplyr::filter(Sex == "M") %>%
-  dplyr::group_by(Weight) %>%
-  dplyr::summarise(Count = n()) %>%
+read.csv("Data/members.csv") %>% 
+  filter(Grade %in% c("Fr.", "So.", "Jr.", "Sr.")) %>%
+  filter(Sex == "M") %>%
+  group_by(Weight) %>%
+  summarise(Count = n()) %>%
   ggplot(aes(x = as.character(Weight), y = Count)) +
   geom_bar(stat = "identity") +
   labs(title = "All Minnesota High School Grades Distribution by Weight Class", x = "Weight Classes")
 
 read.csv("Sandbox/Data/members.csv") %>% 
-  dplyr::filter(Grade %in% c("Fr.", "So.", "Jr.", "Sr.")) %>%
-  dplyr::filter(Sex == "M") %>%
-  dplyr::mutate(Grade = factor(Grade, levels = c("Fr.", "So.", "Jr.", "Sr."))) %>%
-  dplyr::group_by(Grade, Weight) %>%
-  dplyr::summarise(Count = n()) %>%
+  filter(Grade %in% c("Fr.", "So.", "Jr.", "Sr.")) %>%
+  filter(Sex == "M") %>%
+  mutate(Grade = factor(Grade, levels = c("Fr.", "So.", "Jr.", "Sr."))) %>%
+  group_by(Grade, Weight) %>%
+  summarise(Count = n()) %>%
   ggplot(aes(x = as.character(Weight), y = Count)) +
   geom_bar(stat = "identity") +
   facet_wrap(Grade~., nrow = 2) +
@@ -29,30 +32,30 @@ read.csv("Sandbox/Data/members.csv") %>%
 
  
 read.csv("Sandbox/Data/members.csv") %>% 
-  dplyr::filter(Grade %in% c("7th", "8th")) %>%
-  dplyr::group_by(Weight) %>%
-  dplyr::summarise(Count = n()) %>%
+  filter(Grade %in% c("7th", "8th")) %>%
+  group_by(Weight) %>%
+  summarise(Count = n()) %>%
   ggplot(aes(x = as.character(Weight), y = Count)) +
   geom_bar(stat = "identity") +
   labs(title = "Minnesota 7th/8th Grade (Wrestling High School) Distribution by Weight Class", x = "Weight Classes")
 
 read.csv("Sandbox/Data/members.csv") %>% 
-  dplyr::filter(Sex == "M") %>%
-  dplyr::filter(Grade %in% c("Fr.", "So.", "Jr.", "Sr.")) %>%
-  dplyr::pull(Weight) %>%
+  filter(Sex == "M") %>%
+  filter(Grade %in% c("Fr.", "So.", "Jr.", "Sr.")) %>%
+  pull(Weight) %>%
   summary()
 
 
 read.csv("Sandbox/Data/members.csv") %>% 
-  #dplyr::filter(Sex == "M") %>%
-  dplyr::filter(Grade %in% c("So.")) %>%
-  dplyr::pull(Weight) %>%
+  #filter(Sex == "M") %>%
+  filter(Grade %in% c("So.")) %>%
+  pull(Weight) %>%
   summary()
 
 read.csv("Sandbox/Data/members.csv") %>% 
-  dplyr::inner_join(read.csv("Sandbox/Data/teams.csv")) %>%
-  dplyr::filter(Grade %in% c("Fr.", "So.", "Jr.", "Sr.")) %>%
-  dplyr::group_by(Team) %>%
-  dplyr::summarise(AverageWeight = mean(Weight), MedianWeight = median(Weight), TeamSize = n()) %>%
-  dplyr::arrange(AverageWeight) %>% 
+  inner_join(read.csv("Sandbox/Data/teams.csv")) %>%
+  filter(Grade %in% c("Fr.", "So.", "Jr.", "Sr.")) %>%
+  group_by(Team) %>%
+  summarise(AverageWeight = mean(Weight), MedianWeight = median(Weight), TeamSize = n()) %>%
+  arrange(AverageWeight) %>% 
   print(n = 250)
